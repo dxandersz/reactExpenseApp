2c734012847fe935051bfa9f0979b3c00063603e
David Anderson <David.Anderson@curebase.com>
2022-06-18 18:31:44 -0700

    first state example

--- code/01-working-with-state/src/components/Expenses/ExpenseItem.js
+++ code/01-working-with-state/src/components/Expenses/ExpenseItem.js
@@ -5,16 +5,15 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // function clickHandler() {}
   const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
-  
+
   const clickHandler = () => {
-    setTitle('Updated!');
+    setTitle('Title Updated!!!');
     console.log(title);
   };
 
   return (
+
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
